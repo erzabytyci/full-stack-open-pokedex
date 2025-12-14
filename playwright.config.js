e624be7c3411c3500d1134294db207469f7830fa
erzabytyci <151072186+erzabytyci@users.noreply.github.com>
2025-12-14 20:35:56 +0200

    Fix eslint

--- playwright.config.js
+++ playwright.config.js
@@ -2,13 +2,13 @@
 const { defineConfig } = require('@playwright/test')
 
 module.exports = defineConfig({
-    testDir: './e2e-tests',
-    use: {
-        baseURL: 'http://127.0.0.1:5000'
-    },
-    webServer: {
-        command: 'npm run start-prod',
-        url: 'http://127.0.0.1:5000',
-        reuseExistingServer: !process.env.CI
-    }
+  testDir: './e2e-tests',
+  use: {
+    baseURL: 'http://127.0.0.1:5000'
+  },
+  webServer: {
+    command: 'npm run start-prod',
+    url: 'http://127.0.0.1:5000',
+    reuseExistingServer: !process.env.CI
+  }
 })
